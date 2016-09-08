653cac584366a368b36068cb2769901b84498096
root <root@10-19-185-19.(none)>
2016-09-08 14:38:17 +0800

    io

--- base/TestFileWriter.java
+++ base/TestFileWriter.java
@@ -8,6 +8,14 @@ public class TestFileWriter{
 			for (int i=0;i<6000;i++){
 				fw.write(i);
 			}
+			fr = new FileReader("file.txt");
+			int j = 0;
+			int num = 0;
+			while((j=fr.read())!= -1){
+				System.out.print((char)j + "\t");
+				num++;
+			}
+			System.out.println(String.format("一共读取了{%d}个字符串", num));
 		
 		}catch(Exception e){
 			e.printStackTrace();
